1326f5cf7d470314c6c6df2c90363fd372668d54
xiongchun <307916217@qq.com>
2020-02-21 16:52:47 +0800

    update

--- pangu-external-config/src/main/java/com/github/pangu/learning/ConfigApplication.java
+++ pangu-external-config/src/main/java/com/github/pangu/learning/ConfigApplication.java
@@ -2,8 +2,10 @@ package com.github.pangu.learning;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com.github.pangu")
 public class ConfigApplication {
 
 	public static void main(String[] args) {
