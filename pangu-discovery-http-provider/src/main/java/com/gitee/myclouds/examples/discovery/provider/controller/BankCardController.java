52ea1dc9279e527ddb03351d31ca428ca54c9a3e
xiongchun <307916217@qq.com>
2020-02-22 22:46:13 +0800

    增加springcloud http服务提供者

---
+++ pangu-discovery-http-provider/src/main/java/com/gitee/myclouds/examples/discovery/provider/controller/BankCardController.java
@@ -0,0 +1,33 @@
+package com.gitee.myclouds.examples.discovery.provider.controller;
+
+import java.math.BigDecimal;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.gitee.myclouds.examples.discovery.provider.model.BankCard;
+
+/**
+ * 银行卡服务
+ * @author xiongchun
+ *
+ */
+@RestController
+@RequestMapping("bankCard")
+public class BankCardController {
+	
+	@Autowired
+	DiscoveryClient discoveryClient;
+	
+	@GetMapping("findCardInfo")
+	public BankCard findCardInfo(String cardNo, HttpServletRequest request) {
+		System.out.println(cardNo);
+		BankCard bankCard = new BankCard().setCardNo("5301001").setName("XC").setBalance(BigDecimal.valueOf(1000000L));
+		return bankCard;
+	}
+}
