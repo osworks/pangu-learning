493715f96ee2b1a15a3357a9e90e59cf30cf042b
xiongchun <307916217@qq.com>
2020-03-07 16:43:22 +0800

    完善日志输出

--- pangu-discovery-http-consumer/src/main/java/com/github/pangu/learning/discovery/http/consumer/controller/BankCardConsumerController.java
+++ pangu-discovery-http-consumer/src/main/java/com/github/pangu/learning/discovery/http/consumer/controller/BankCardConsumerController.java
@@ -6,12 +6,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.github.pangu.learning.discovery.http.consumer.feignclient.BankCardFeignClient;
 import com.github.pangu.learning.discovery.http.consumer.model.BankCard;
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * 演示基于RestTemplate消费服务
  * @author xiongchun
  *
  */
+@Slf4j
 @RestController
 @RequestMapping("bankCard")
 public class BankCardConsumerController {
@@ -21,8 +23,9 @@ public class BankCardConsumerController {
 
 	@GetMapping("findCardInfo")
 	public BankCard findCardInfo(String cardNo) {
+	    log.info("入参 cardNo:{}", cardNo);
 		BankCard bankCard = bankCardFeignClient.findCardInfo(cardNo);
-		System.out.println(bankCard);
+		log.info("出参 bankCard:{}", bankCard);
 		return bankCard;
 	}
 
--- pangu-discovery-http-provider/src/main/java/com/github/pangu/learning/discovery/http/provider/controller/BankCardController.java
+++ pangu-discovery-http-provider/src/main/java/com/github/pangu/learning/discovery/http/provider/controller/BankCardController.java
@@ -1,21 +1,20 @@
 package com.github.pangu.learning.discovery.http.provider.controller;
 
 import java.math.BigDecimal;
-
-import javax.servlet.http.HttpServletRequest;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.github.pangu.learning.discovery.http.provider.model.BankCard;
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * 银行卡服务
  * @author xiongchun
  *
  */
+@Slf4j
 @RestController
 @RequestMapping("bankCard")
 public class BankCardController {
@@ -24,9 +23,10 @@ public class BankCardController {
 	DiscoveryClient discoveryClient;
 	
 	@GetMapping("findCardInfo")
-	public BankCard findCardInfo(String cardNo, HttpServletRequest request) {
-		System.out.println(cardNo);
+	public BankCard findCardInfo(String cardNo) {
+	    log.info("入参 cardNo:{}", cardNo);
 		BankCard bankCard = new BankCard().setCardNo("5301001").setName("XC").setBalance(BigDecimal.valueOf(1000000L));
+		log.info("出参 bankCard:{}", bankCard);
 		return bankCard;
 	}
 }
